c30e499c63c1232229630353e75619e28f79d96d
myyyyw <67189829+myyyyw@users.noreply.github.com>
2023-03-09 19:51:02 +0800

    Add files via upload

---
+++ Download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'Start downloading'
+
+git lfs install
+git clone https://huggingface.co/datasets/myyyyw/NTLNP
+
+# if you want to clone without large files – just their pointers
+# prepend your git clone with the following env var:
+# GIT_LFS_SKIP_SMUDGE=1
+
+echo 'Download finished'
